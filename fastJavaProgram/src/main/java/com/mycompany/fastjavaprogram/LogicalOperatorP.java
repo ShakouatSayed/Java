274496cea56d019942cdb4b0b8ecdca65d54eb94
Sakhawat Hossain Sayed <sayedckb61@gmail.com>
2023-09-08 19:56:11 +0600

    first commit

---
+++ fastJavaProgram/src/main/java/com/mycompany/fastjavaprogram/LogicalOperatorP.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fastjavaprogram;
+import PackageFive.LogicalOperator;
+/**
+ *
+ * @author SHAKOUAT HOSSEN
+ */
+public class LogicalOperatorP {
+    public static void main(String[] args){
+        LogicalOperator a = new LogicalOperator();
+        a.andOperator();
+        a.orOperator();
+        a.notOperator();
+    }
+}
---
+++ fastJavaProgram/src/main/java/com/mycompany/fastjavaprogram/if_else1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fastjavaprogram;
+/**
+ *
+ * @author SHAKOUAT HOSSEN
+ */
+public class if_else1 {
+    public static void main(String[] args){
+        int test = 3;
+        switch (test) {
+            case 3 -> System.out.println("Hello");
+            case 2 -> System.out.println("Hi");
+            case 1 -> System.out.println("Good");
+            default -> System.out.println("not Display");
+        }
+    }
+}
